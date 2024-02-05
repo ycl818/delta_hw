8f8ca082b5c6f8d0462d6927adf4e2f3af87c8d9
yichien <kevinyichienlee@gmail.com>
2024-02-06 07:26:55 +0800

    getUser func 3

--- src/components/AuthComponents/PersistLogin.jsx
+++ src/components/AuthComponents/PersistLogin.jsx
@@ -1,14 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useLayoutEffect } from "react";
 import { useState } from "react";
 import useRefreshToken from "../../hooks/useRefreshToken";
 import useAuth from "../../hooks/useAuth";
 import { Outlet } from "react-router-dom";
 import PuffLoader from "../Loader/PuffLoader";
+import axios from "../../api/axios";
 
 const PersistLogin = () => {
   const [isLoading, setIsLoading] = useState(true);
   const refresh = useRefreshToken();
-  const { auth } = useAuth();
+  const { auth, setAuth } = useAuth();
+
+  useLayoutEffect(() => {
+    const getUser = async () => {
+      try {
+        const response = axios.get("/auth/login/success", {
+          withCredentials: true,
+        });
+        console.log("🚀 ~ getUser ~ response:", response);
+        const username = response.data?.user?.name;
+        const mail = response.data?.user?.account;
+        const userphoto = response.data?.user?.photo;
+
+        setAuth({ username, mail, userphoto });
+        setIsLoading(false);
+      } catch (error) {
+        console.log("🚀 ~ getUser ~ error:", error);
+      }
+    };
+
+    getUser();
+  }, [setAuth]);
 
   useEffect(() => {
     const verifyRefreshToken = async () => {
--- src/components/AuthComponents/RequireAuth.jsx
+++ src/components/AuthComponents/RequireAuth.jsx
@@ -1,32 +1,11 @@
-import React, { useLayoutEffect } from "react";
+import React from "react";
 import useAuth from "../../hooks/useAuth";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
-import axios from "../../api/axios";
 
 const RequireAuth = () => {
-  const { auth, setAuth } = useAuth();
+  const { auth } = useAuth();
   console.log("🚀 ~ RequireAuth ~ auth:", auth);
   const location = useLocation();
-
-  useLayoutEffect(() => {
-    const getUser = async () => {
-      try {
-        const response = axios.get("/auth/login/success", {
-          withCredentials: true,
-        });
-        console.log("🚀 ~ getUser ~ response:", response);
-        const username = response.data?.user?.name;
-        const mail = response.data?.user?.account;
-        const userphoto = response.data?.user?.photo;
-
-        setAuth({ username, mail, userphoto });
-      } catch (error) {
-        console.log("🚀 ~ getUser ~ error:", error);
-      }
-    };
-
-    getUser();
-  }, [setAuth]);
 
   return auth?.mail ? (
     <Outlet />
