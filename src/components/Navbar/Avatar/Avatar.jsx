c56a0d11dfa7ed4b61f52eb80552dba2304b2681
yichien <kevinyichienlee@gmail.com>
2024-02-06 08:37:08 +0800

    adj nav avatar

--- src/components/Navbar/Avatar/Avatar.jsx
+++ src/components/Navbar/Avatar/Avatar.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./Avatar.scss";
 import { HiOutlineChevronDown } from "react-icons/hi";
+import useAuth from "../../../hooks/useAuth";
 
 const Avatar = () => {
+  const { userphoto } = useAuth();
+
   return (
     <div className="avatar_wrapper">
       <img
         className="avatar__image"
-        src="https://lh3.googleusercontent.com/a/ACg8ocIA7zmOBAB_Z6KKHejBXAkjRYe1PASZecMV41rAj5mzSQ=s96-c"
-        alt=""
+        src={userphoto}
+        alt="https://lh3.googleusercontent.com/a/ACg8ocIA7zmOBAB_Z6KKHejBXAkjRYe1PASZecMV41rAj5mzSQ=s96-c"
         width={40}
         height={40}
       />
